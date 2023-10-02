251583edd7481d93c04f0b270d8e3f632befaf4d
StevenLiu <stevenliu986@gmail.com>
2023-10-02 14:46:56 +1100

    generate an array

--- tutorial04/src/App.jsx
+++ tutorial04/src/App.jsx
@@ -11,11 +11,34 @@ function App() {
   //   );
   // }, []);
 
+  // const [guesses, setGuesses] = useState(Array(6).fill(null));
+
+  // const randomIndex = Math.floor(Math.random() * mockData.length);
+  // const randomWord = mockData[randomIndex];
+
+  const guesses = new Array(6).fill("H");
+
   return (
     <>
-      <h1>React Tutorial04</h1>
+      {guesses.map((guess, index) => (
+        <Line key={index} guess={guess} />
+      ))}
     </>
   );
 }
 
 export default App;
+
+function Line({ guess }) {
+  const tiles = [];
+  for (let i = 0; i < 5; i++) {
+    const char = guess[i];
+    tiles.push(
+      <div key={i} className="tile">
+        {char}
+      </div>
+    );
+  }
+
+  return <div className="line"> {tiles}</div>;
+}
