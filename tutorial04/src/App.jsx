1af11b8a7b0d7f9e52f4f3e4d292aeaa5cde99ea
StevenLiu <stevenliu986@gmail.com>
2023-10-02 13:00:42 +1100

    process data

--- tutorial04/src/App.jsx
+++ tutorial04/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { mockData } from "./mockData";
+import "./App.css";
+
+// const API_URL = "https://api.frontendexpert.io/api/fe/wordle-words";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   fetch(API_URL, { method: "GET", mode: "no-cors" }).then((response) =>
+  //     console.log(response)
+  //   );
+  // }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Tutorial04</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
